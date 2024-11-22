eaa36979c4b93d176d02148d5cf79623ad51753a
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-22 14:09:48 +0100

    added

--- freetone/UI/UserRegistration/RegisterWithEmailViewController.swift
+++ freetone/UI/UserRegistration/RegisterWithEmailViewController.swift
@@ -11,10 +11,13 @@ class RegisterWithEmailViewController: UIViewController {
     
     //MARK: - Objects -
     ////TEXTFIELD
-    let emailTextField = FloatingTextField(placeHolder: "email", isSecureTextEntry: false, radius: 5, background: .clear, borderWidth: 2, borderColor: .gray)
+    let emailTextField = FloatingTextField(placeHolder: "Enter your valid email address", isSecureTextEntry: false, radius: 5, background: .clear, borderWidth: 2, borderColor: .red)
     
     ////TEXTFIELD
-    let passwordTextField = FloatingTextField(placeHolder: "password", isSecureTextEntry: true, radius: 5, background: .clear, borderWidth: 2, borderColor: .gray)
+    let passwordTextField = FloatingTextField(placeHolder: "Create your password", isSecureTextEntry: true, radius: 5, background: .clear, borderWidth: 2, borderColor: .red)
+    
+    ////BUTTON
+    let regButton = Button(image: UIImage(), text: "Start calling & texting", btnTitleColor: .gray, backgroundColor: .darkGray, radius: 10, imageColor: .clear, borderWidth: 0, borderColor: UIColor.clear.cgColor)
 
     //MARK: - Life Cycle -
     override func viewDidLoad() {
@@ -26,8 +29,9 @@ class RegisterWithEmailViewController: UIViewController {
     func setSubviewsAndLayout() {
         view.addSubview(emailTextField)
         view.addSubview(passwordTextField)
+        view.addSubview(regButton)
         NSLayoutConstraint.activate([
-            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200),
+            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
             emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             emailTextField.heightAnchor.constraint(equalToConstant: 50),
@@ -38,6 +42,12 @@ class RegisterWithEmailViewController: UIViewController {
             passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             passwordTextField.heightAnchor.constraint(equalToConstant: 50),
             passwordTextField.widthAnchor.constraint(equalToConstant: 300),
+            
+            regButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 40),
+            regButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            regButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            regButton.heightAnchor.constraint(equalToConstant: 55),
+            regButton.widthAnchor.constraint(equalToConstant: 300)
         ])
         emailTextField.translatesAutoresizingMaskIntoConstraints = false
         passwordTextField.translatesAutoresizingMaskIntoConstraints = false
--- freetone/UI/UserRegistration/SignInViewController.swift
+++ freetone/UI/UserRegistration/SignInViewController.swift
@@ -8,10 +8,100 @@
 import UIKit
 
 class SignInViewController: UIViewController {
-
+    
+    //MARK: - Objects -
+    ////UIImageView
+    fileprivate lazy var appLogo: UIImageView = {
+        let logo = UIImageView(image: UIImage(named: "logo"))
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.tintColor = UIColor(red: 0/255, green: 255/255, blue: 230/255, alpha: 1.0)
+        logo.contentMode = .scaleAspectFit
+        return logo
+    }()
+    
+    ////LABEL
+    fileprivate lazy var appLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "FreeTone"
+        label.textColor = UIColor(red: 0/255, green: 255/255, blue: 230/255, alpha: 1.0)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    ////STACKVIEW
+    fileprivate lazy var stackView: UIStackView = {
+        let stack = UIStackView(arrangedSubviews: [appLogo, appLabel])
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .horizontal
+        stack.spacing = -80
+        stack.alignment = .center
+        return stack
+    }()
+    
+    ////LABEL
+    let titleLabel = Label(label: "Welcome back! \n\n Log in below.", textColor: .white, font: UIFont.systemFont(ofSize: 18, weight: .black))
+    
+    ////TEXTFIELD
+    let emailTextField = FloatingTextField(placeHolder: "Enter your valid email address", isSecureTextEntry: false, radius: 5, background: .clear, borderWidth: 2, borderColor: .red)
+    
+    ////TEXTFIELD
+    let passwordTextField = FloatingTextField(placeHolder: "Create your password", isSecureTextEntry: true, radius: 5, background: .clear, borderWidth: 2, borderColor: .red)
+    
+    ////BUTTON
+    let forgotPasswordBtn = Button(image: UIImage(), text: "Forgot your password?", btnTitleColor: UIColor(red: 0/255, green: 255/255, blue: 230/255, alpha: 1.0), backgroundColor: .clear, radius: 0, imageColor: .clear, borderWidth: 0, borderColor: UIColor.clear.cgColor)
+    
+    ////BUTTON
+    let loginButton = Button(image: UIImage(), text: "Log in", btnTitleColor: .gray, backgroundColor: .darkGray, radius: 10, imageColor: .clear, borderWidth: 0, borderColor: UIColor.clear.cgColor)
+    
+    //MARK: - Life Cycle -
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setSubviewsAndLayout()
     }
     
+    // MARK: - Subviews and Layout -
+    func setSubviewsAndLayout() {
+        view.addSubview(stackView)
+        view.addSubview(titleLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(forgotPasswordBtn)
+        view.addSubview(loginButton)
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
+            
+            titleLabel.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 30),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            emailTextField.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 40),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            emailTextField.heightAnchor.constraint(equalToConstant: 50),
+            emailTextField.widthAnchor.constraint(equalToConstant: 300),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            passwordTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 50),
+            passwordTextField.widthAnchor.constraint(equalToConstant: 300),
+            
+            forgotPasswordBtn.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            forgotPasswordBtn.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            loginButton.topAnchor.constraint(equalTo: forgotPasswordBtn.bottomAnchor, constant: 20),
+            loginButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            loginButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            loginButton.heightAnchor.constraint(equalToConstant: 55),
+            loginButton.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        forgotPasswordBtn.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+    }
 }
