f13846858b6842ccd965fe84b7a4d047c3e68233
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-22 20:22:56 +0100

    added

---
+++ freetone/UI/TabBarVC/TabBars/Segments/AllView.swift
@@ -0,0 +1,13 @@
+//
+//  AllView.swift
+//  freetone
+//
+//  Created by Mac on 22/11/2024.
+//
+
+import UIKit
+
+//MARK: - Setting the objects views and its properties
+class AllView: UIView {
+    
+}
---
+++ freetone/UI/TabBarVC/TabBars/Segments/MissedView.swift
@@ -0,0 +1,13 @@
+//
+//  MissedView.swift
+//  freetone
+//
+//  Created by Mac on 22/11/2024.
+//
+
+import UIKit
+
+//MARK: - Setting the objects views and its properties
+class MissedView: UIView {
+    
+}
