745cf951e73d269ec3b780083bb1d9ae9c35d4a3
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-23 17:41:50 +0100

    added

--- freetone/NetworkManager.swift
+++ freetone/NetworkManager.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import Appwrite
+import NIO
+import UIKit
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    //MARK: - PERSIST USER SESSION/LOGIN SESSION -
+    func checkStoredSession() async {
+        
+    }
+    
+    //MARK: - CHECK USER SESSION -
+    func checkSession() async {
+       
+    }
+    
+    //MARK: - REGISTER USER -
+    func register() async {
+        
+    }
+    
+    //MARK: - LOGIN USER -
+    func login() async {
+        
+    }
+    
+    //MARK: - FETCHING USER -
+    func getUser() async {
+       
+    }
+    
+    //MARK: - LOGING USER OUT -
+    func logout() async {
+        
+    }
+}
+
