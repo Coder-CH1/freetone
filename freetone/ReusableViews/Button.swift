c9e7ad6c599b1ceb873848440bd78844e3b310f8
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-11-24 23:14:08 +0100

    added

--- freetone/ReusableViews/Button.swift
+++ freetone/ReusableViews/Button.swift
@@ -8,9 +8,15 @@ import UIKit
 
 // MARK: - ReusableObject -
 class Button: UIButton {
+    var text: String {
+        didSet {
+            setTitle(text, for: .normal)
+        }
+    }
     
     // MARK: - Object property and value initialization -
     init(image: UIImage?, text: String, btnTitleColor: UIColor, backgroundColor: UIColor, radius: CGFloat, imageColor: UIColor, borderWidth: Int, borderColor: CGColor) {
+        self.text = text
         super.init(frame: .zero)
         self.translatesAutoresizingMaskIntoConstraints = false
         self.setImage(image, for: .normal)
@@ -19,6 +25,7 @@ class Button: UIButton {
         self.backgroundColor = backgroundColor
         self.layer.cornerRadius = radius
         self.tintColor = imageColor
+        self.clipsToBounds = true
         self.isUserInteractionEnabled = true
         self.layer.borderWidth = CGFloat(borderWidth)
         self.layer.borderColor = borderColor
