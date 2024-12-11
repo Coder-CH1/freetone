84b2d72a20831ee341872b360d1f4f17bc15adbf
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-11 15:19:51 +0100

    added

--- freetone/NetworkManager.swift
+++ freetone/NetworkManager.swift
@@ -16,6 +16,15 @@ class NetworkManager {
     
     private init() {}
     
+    let client = Client()
+        .setEndpoint("https://cloud.appwrite.io/v1") // Your API Endpoint
+        .setProject("6746f883000a071f1c3f") // Your project ID
+
+    lazy var account: Account = {
+        let account = Account(client)
+        return account
+    }()
+    
     //MARK: - PERSIST USER SESSION/LOGIN SESSION -
     func checkStoredSession() async {
         
@@ -28,12 +37,31 @@ class NetworkManager {
     
     //MARK: - REGISTER USER -
     func register() async {
-        
+        do {
+            _  = try await account.create(
+                userId: ID.unique(),
+                email: "",
+                password: ""
+            )
+        } catch {
+            await MainActor.run {
+                print(error.localizedDescription)
+            }
+        }
     }
     
     //MARK: - LOGIN USER -
     func login() async {
-        
+        do {
+            let session = try await account.createEmailPasswordSession(
+                email: "",
+                password: ""
+            )
+        } catch {
+            await MainActor.run {
+                print(error.localizedDescription)
+            }
+        }
     }
     
     //MARK: - FETCHING USER -
@@ -43,7 +71,14 @@ class NetworkManager {
     
     //MARK: - LOGING USER OUT -
     func logout() async {
-        
+        do {
+            _  = try await account.deleteSession(sessionId: "current")
+            UserDefaults.standard.removeObject(forKey: "sessionToken")
+        } catch {
+            await MainActor.run {
+                 print(error.localizedDescription)
+            }
+        }
     }
 }
 
