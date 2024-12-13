75359c664bd96fe10ac31e2d25ac7615622885d7
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-13 15:24:42 +0100

    added

--- freetone/ReusableViews/ProfileTableView/ProfileTableView.swift
+++ freetone/ReusableViews/ProfileTableView/ProfileTableView.swift
@@ -112,7 +112,7 @@ class ProfileTableView: UITableView, UITableViewDataSource, UITableViewDelegate 
             let label = UILabel()
             label.text = AuthManager.shared.email
             label.textColor = UIColor(red: 0/255, green: 255/255, blue: 230/255, alpha: 1.0)
-            label.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+            label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
             label.translatesAutoresizingMaskIntoConstraints = false
             
             headerView.addSubview(img)
