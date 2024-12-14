a8348f98b4f298f42f352ca91a5836cc397f2372
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-14 14:50:19 +0100

    added

--- freetone/APPWRITE/DatabaseManager.swift
+++ freetone/APPWRITE/DatabaseManager.swift
@@ -15,4 +15,42 @@ class DatabaseManager {
     static let shared = DatabaseManager()
     
     private init() {}
+    
+    //MARK: - Retrieve values from info.plist -
+    var projectID: String {
+        guard let id = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_PROJECT_ID") as? String else {
+            fatalError("")
+        }
+        return id
+    }
+    
+    //MARK: - Retrieve values from info.plist -
+    var endPoint: String {
+        guard let url = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_ENDPOINT") as? String else {
+            fatalError("")
+        }
+        return url
+    }
+    
+    //MARK: - Lazy initialization of client -
+    lazy var client: Client = {
+        let client = Client()
+            .setEndpoint(endPoint)
+            .setProject(projectID)
+        return client
+    }()
+
+    //MARK: - Lazy initialization of account -
+    lazy var account: Account = {
+        let account = Account(client)
+        return account
+    }()
+    
+    func createCollection() async {
+        
+    }
+    
+    func fetchUser() async {
+        
+    }
 }
