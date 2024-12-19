c182cbdd3c96933f439ee462c51a49256c8de969
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-19 15:09:27 +0100

    added

--- freetone/Extensions+Helper/BaseViewController.swift
+++ freetone/Extensions+Helper/BaseViewController.swift
@@ -24,31 +24,28 @@ class BaseViewController: UIViewController {
     
     func navigateToDialVC() {
         let vc = DialerViewController()
-        self.title = title
-        
-        let backButton = UIButton(type: .system)
-        backButton.setImage(UIImage(systemName: "xmark"), for: .normal)
-        backButton.setTitle("Dial a number", for: .normal)
-        backButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .black)
-        
-        backButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
-        backButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -25, bottom: 0, right: 0)
-        
-        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
-        
-        let backItem = UIBarButtonItem(customView: backButton)
-        self.navigationItem.leftBarButtonItem = backItem
-    
-            let contactButton = UIBarButtonItem(
-                image: UIImage(systemName: "person.crop.square"),
-                style: .plain,
-                target: self,
-                action: #selector(backButtonTapped)
-            )
-        
-            vc.navigationItem.rightBarButtonItem = contactButton
-        navigationController?.navigationBar.tintColor = .white
-        navigationController?.pushViewController(vc, animated: false)
+              navigationController?.pushViewController(vc, animated: false)
+              let backButton = UIButton(type: .system)
+              backButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+              backButton.setTitle("Dial a number", for: .normal)
+              backButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .black)
+              
+              backButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -10, bottom: 0, right: 0)
+              backButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -25, bottom: 0, right: 0)
+              
+              backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+              
+              let backItem = UIBarButtonItem(customView: backButton)
+              vc.navigationItem.leftBarButtonItem = backItem
+              
+              let contactButton = UIBarButtonItem(
+                  image: UIImage(systemName: "person.crop.square"),
+                  style: .plain,
+                  target: self,
+                  action: #selector(contactButtonTapped)
+              )
+              vc.navigationItem.rightBarButtonItem = contactButton
+              navigationController?.navigationBar.tintColor = .white
     }
     
     @objc func backButtonTapped() {
