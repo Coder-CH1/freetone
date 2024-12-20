6de14012be4158826ccd33a99a08ba4bbb4870f1
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-12-20 10:49:29 +0100

    added

--- freetone/Appwrite/Model.swift
+++ freetone/Appwrite/Model.swift
@@ -7,12 +7,14 @@
 
 import Foundation
 
-class User {
+//MARK: - Data Model -
+struct User {
     var email: String
     var id: String
     var phoneNumbers: String
     var profileImageUrl: String
     
+    //MARK: - Initialize -
     init(email: String, id: String,phoneNumbers: String, profileImageUrl: String) {
         self.email = email
         self.id = id
@@ -27,6 +29,32 @@ class User {
             "id": self.id,
             "phoneNumbers": self.phoneNumbers,
             "profileImageUrl": self.profileImageUrl
+        ]
+    }
+}
+
+//MARK: - Data Model -
+struct Message {
+    var senderPhoneNumber: String
+    var recipientPhoneNumber: String
+    var messageBody: String
+    var time: String
+    
+    //MARK: - Initialize -
+    init(senderPhoneNumber: String, recipientPhoneNumber: String, messageBody: String, time: String) {
+        self.senderPhoneNumber = senderPhoneNumber
+        self.recipientPhoneNumber = recipientPhoneNumber
+        self.messageBody = messageBody
+        self.time = time
+    }
+    
+    //MARK: - Convert user object to a dictionary -
+    func toDictionary() -> [String: Any] {
+        return [
+            "senderPhoneNumber": self.senderPhoneNumber,
+            "recipientPhoneNumber": self.recipientPhoneNumber,
+            "messageBody": self.messageBody,
+            "time": self.time
         ]
     }
 }
